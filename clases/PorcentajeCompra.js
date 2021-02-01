c50729ffeb75acae193ce0f2fc4d936e0f9700e8
Ruby Lorena <ruby-loren@hotmail.com>
2021-01-31 20:00:04 -0500

    Numeros

---
+++ clases/PorcentajeCompra.js
@@ -0,0 +1,7 @@
+let valordelobjeto, valorpagado, porcentaje
+
+valordelobjeto = +prompt("Ingrese valor del objeto")
+valorpagado = +prompt ("Ingrese el valor pagado")
+porcentaje = (valorpagado*100)/valordelobjeto
+
+alert("El porcentaje fue " + (100 - porcentaje))
---
+++ clases/UltimaCifra.js
@@ -0,0 +1,6 @@
+let numero, residuo
+
+numero = +prompt("Ingrese número")
+residuo = numero % 10
+
+alert("La última cifra es " + residuo)
