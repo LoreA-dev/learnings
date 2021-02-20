8ed2eb16bb4b350ba766b1e2e216c8b7569146f9
LoreA-dev <ruby-loren@hotmail.com>
2021-02-19 22:09:36 -0500

    cajero

--- ProgramaciónBásica/cajero.js
+++ ProgramaciónBásica/cajero.js
@@ -5,40 +5,48 @@ class billet {
     }
 }
 
-
 function withdrawal() {
-    for (let billet of atm) {
-        console.log(billet)
-        // if (texto > 0) {
-        //     let div = Math.floor(texto/this.value)
+    result.innerText = ""
+    giver = []
+    let textBox = document.getElementById("dinero")
+    let money = parseInt(textBox.value)
 
-        //     if (div > this.amount) {
+    for (let billets of atm) {
+        if (money > 0) {
+            div = Math.floor(money / billets.value)
+            if (div > billets.amount) {
+                moneyToDeliver = billets.amount
+            }
+            else {
+                moneyToDeliver = div
+            }
+            giver.push(new billet(billets.value, moneyToDeliver))
+            money -= (billets.value * moneyToDeliver)
+        }
+    }
 
-        //     }
-        // }
+    if (money > 0) {
+        result.innerHTML = "Valor a entregar invalido"
+        return
+    }else{
+        for (var givers of giver) {
+            console.log(result)
+            if (givers.amount > 0) {
+                result.innerHTML += givers.amount + " billetes de $" + givers.value + "<br />"
+            }
+        }
     }
 }
 
-
 let atm = []
 let giver = []
-atm.push(new billet(50, 3))
-atm.push(new billet(20, 2))
-atm.push(new billet(10, 2))
-
-
-let papers = 0
-
+atm.push(new billet(50, 10))
+atm.push(new billet(20, 5))
+atm.push(new billet(10, 5))
+atm.push(new billet(5, 3))
+let div = 0
+let moneyToDeliver = 0
 
 let extraer = document.getElementById("boton")
-let texto = document.getElementById("dinero")
+let result = document.getElementById("resultado")
 extraer.addEventListener("click", withdrawal)
-
-let money = parseInt(texto.value)
-
-
-
-
-// for (let money of atm){
-//     money.show()
-// }
